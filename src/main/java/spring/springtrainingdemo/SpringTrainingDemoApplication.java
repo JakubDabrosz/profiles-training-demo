3f7fd9696923651549c6fcb4e67ca7b1427bf187
Jakub Dabrosz <j4.dabrowski@gmail.com>
2022-12-04 13:21:02 +0100

    add repo to remote

---
+++ src/main/java/spring/springtrainingdemo/SpringTrainingDemoApplication.java
@@ -0,0 +1,13 @@
+package spring.springtrainingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrainingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/springtrainingdemo/SpringTrainingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springtrainingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
